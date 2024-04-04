331ed8a97af866acd6ac728a3ad73fcc258ac3f5
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2024-04-04 10:30:01 -0500

    update hash function

--- src/hash/index.ts
+++ src/hash/index.ts
@@ -22,21 +22,27 @@ export function insertHashPepper(insertString: string, dataType: number, pepperS
     return newString.join('');
 }
 
+
+export interface HashDataConfig {
+    dataType: number,
+    pepperString: string[]
+}
+
 /** this function hash any arbitrary JavaScript object, adds a pepper and then hashes the data again with a salt.
  * @dataType this is used to provide variability when hashing data
 */
-export async function hashData(inputData: any, dataType: number, pepperString: string[]): Promise<string> {
-    const tempData = insertHashPepper(hash(inputData), dataType, pepperString)
+export async function hashData(inputData: any, config: HashDataConfig): Promise<string> {
+    const tempData = insertHashPepper(hash(inputData), config.dataType, config.pepperString)
     const hash1 = hash({ data: tempData + getSaltRounds() })
-    const temp2 = insertHashPepper(hash1, dataType + 2, pepperString)
+    const temp2 = insertHashPepper(hash1, config.dataType + 2, config.pepperString)
     return await bcrypt.hash(temp2, getSaltRounds());
 }
 
 /** used to verify if the hash string is derived from the input data */
-export async function compareHash(inputData: any, dataType: number, hashString: string, pepperString: string[]): Promise<boolean> {
-    const tempData = insertHashPepper(hash(inputData), dataType, pepperString)
+export async function compareHash(inputData: any, hashString: string, config: HashDataConfig): Promise<boolean> {
+    const tempData = insertHashPepper(hash(inputData), config.dataType, config.pepperString)
     const hash1 = hash({ data: tempData + getSaltRounds() })
-    const temp2 = insertHashPepper(hash1, dataType + 2, pepperString)
+    const temp2 = insertHashPepper(hash1, config.dataType + 2, config.pepperString)
     return await bcrypt.compare(temp2, hashString);
 }
 
--- test/hash.test.ts
+++ test/hash.test.ts
@@ -1,47 +1,50 @@
 import hash from 'object-hash'
 import bcrypt from 'bcrypt';
-import { compareHash, getSaltRounds, hashData, insertHashPepper } from '../src/hash'
+import { HashDataConfig, compareHash, getSaltRounds, hashData, insertHashPepper } from '../src/hash';
+import { v4 } from 'uuid';
 
 describe('Test Hash Functions', () => {
     const OLD_ENV = process.env;
-    const pepperArray = ["test", "default", "other"]
-
+    const pepperArray = ["test", "default", "other"];
     beforeEach(() => {
-        jest.resetModules()
-        jest.clearAllMocks()
+        jest.resetModules();
+        jest.clearAllMocks();
         process.env = { ...OLD_ENV };
     });
-
     afterAll(() => {
         process.env = OLD_ENV;
     });
-
     it('test salt value', () => {
-        process.env.SERVER_HASH_SALT_ROUNDS = undefined
-        const result1 = getSaltRounds()
-        expect(result1).toBe(0)
-        process.env.SERVER_HASH_SALT_ROUNDS = '5'
-        const result2 = getSaltRounds()
-        expect(result2).toBe(5)
+        process.env.SERVER_HASH_SALT_ROUNDS = undefined;
+        const result1 = getSaltRounds();
+        expect(result1).toBe(0);
+        process.env.SERVER_HASH_SALT_ROUNDS = '5';
+        const result2 = getSaltRounds();
+        expect(result2).toBe(5);
 
-    })
+    });
     it('insertPepper', () => {
         const hashString = "test", dataType = 8;
         const unPepperString = hash(hashString + hashString + dataType);
         const pepperString = insertHashPepper(hashString, dataType, pepperArray);
         expect(unPepperString).not.toBe(pepperString);
         expect(unPepperString.length === pepperString.length).toBeFalsy();
-    })
+    });
     it('hashData', async () => {
-        const testHashData = { test: "test" };
-        const index = 9;
-        const testHash = hash(testHashData);
-        const unPepperString = hash(testHash + testHash + index);
-        const hashTest = await bcrypt.hash(unPepperString, getSaltRounds());
-        const checkHash = await hashData(testHashData, index, pepperArray);
-        expect(hashTest === checkHash).toBeFalsy();
-        expect(await compareHash(testHashData, index, hashTest, pepperArray)).toBeFalsy();
-        expect(await compareHash(testHashData, index, checkHash, pepperArray)).toBeTruthy();
-
-    })
+        for (let i = 0; i < 10; i++) {
+            const testHashData = { test: v4() };
+            const index = 9;
+            const testHash = hash(testHashData);
+            const unPepperString = hash(testHash + testHash + index);
+            const hashTest = await bcrypt.hash(unPepperString, getSaltRounds());
+            const config: HashDataConfig = {
+                dataType: index,
+                pepperString: pepperArray
+            };
+            const checkHash = await hashData(testHashData, config);
+            expect(hashTest === checkHash).toBeFalsy();
+            expect(await compareHash(testHashData, hashTest, config)).toBeFalsy();
+            expect(await compareHash(testHashData, checkHash, config)).toBeTruthy();
+        };
+    });
 })
