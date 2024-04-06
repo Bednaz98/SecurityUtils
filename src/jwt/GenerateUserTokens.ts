2e79bcef8dca241a1b0999170331dc941ec75b64
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2024-04-06 00:14:47 -0500

    update types

--- src/jwt/GenerateUserTokens.ts
+++ src/jwt/GenerateUserTokens.ts
@@ -3,13 +3,13 @@ import { generateJWT } from "./JwtGeneration";
 /** generate access token for a given user
  * @options this can be used to provide any extra properties that are not "standard" for jwts for your use case. These values will be in the payload of the jwt
 */
-export function generateAccessToken(userID: string, issuer: string, keyArray: string[], validTime: number, options?: any, audience?: string) {
+export function generateAccessToken(userID: string, issuer: string, keyArray: string[], validTime: number | string, options?: any, audience?: string) {
     return generateJWT({ userID, ... (options ?? {}) }, issuer, keyArray, { audience, expireTime: validTime, subject: (options?.subject ?? "access") });
 }
 
 /** generate refresh token for a given user
  * @options this can be used to provide any extra properties that are not "standard" for jwts for your use case. These values will be in the payload of the jwt
 */
-export function generateRefreshToken(userID: string, issuer: string, keyArray: string[], validTime: number, options?: any, audience?: string) {
+export function generateRefreshToken(userID: string, issuer: string, keyArray: string[], validTime: number | string, options?: any, audience?: string) {
     return generateJWT({ userID, ...options }, issuer, keyArray, { audience, expireTime: validTime, subject: (options?.subject ?? "refresh") });
 }
